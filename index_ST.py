00ab458c31a2da9c51b96e6ada0a6e3f3e8dfdd2
Shubham Kumar <42334452+imshubhamcodex@users.noreply.github.com>
2023-09-06 00:36:11 +0530

    Add files via upload

--- index_ST.py
+++ index_ST.py
@@ -5,16 +5,83 @@ from datetime import datetime, timedelta
 import mplfinance as mpl
 from datetime import datetime, time as datetime_time
 import time
+import requests
+from pytz import timezone
 
 from mercury_Bot import send_message, send_img
 
 # Fetch data
 def fetch_data(ticker="^NSEI", time_interval="15m"):
     end_date = datetime.today().date()
-    start_date = end_date - timedelta(days=50)
+    start_date = end_date - timedelta(days=7)
     data = yf.download(ticker, start=start_date, end=end_date, interval=time_interval)
     return data
 
+
+def fetch_todays_data_from_YF():
+    current_datetime =  str(datetime.today().date())
+    date_obj = datetime.strptime(current_datetime, "%Y-%m-%d")
+    today_timestamp = str(date_obj.timestamp()).split('.')[0]
+    current_timestamp = str(time.mktime(time.localtime())).split('.')[0]
+    # today_timestamp = str((datetime.now() - timedelta(days=3)).timestamp()).split('.')[0]  # TEST
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "max-age=0",
+        "Sec-Ch-Ua": "\"Not/A)Brand\";v=\"99\", \"Microsoft Edge\";v=\"115\", \"Chromium\";v=\"115\"",
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "\"Windows\"",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.203",
+        "Referer": "https://query1.finance.yahoo.com/v8/finance/chart/%5ENSEI?symbol=%5ENSEI&period1="+ today_timestamp +"&period2=" + current_timestamp + "&useYfid=true&interval=1h&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=pRymmeKo5Qz&corsDomain=finance.yahoo.com"
+    }
+    url = ("https://query1.finance.yahoo.com/v8/finance/chart/%5ENSEI?symbol=%5ENSEI&period1="+ today_timestamp +"&period2=" + current_timestamp + "&useYfid=true&interval=1h&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=pRymmeKo5Qz&corsDomain=finance.yahoo.com")
+    
+    response = requests.get(url,headers=headers)
+    json_data = response.json()
+
+    if 'timestamp' in json_data['chart']['result'][0]:
+        timestamp = json_data['chart']['result'][0]['timestamp']
+        open_prices = json_data['chart']['result'][0]['indicators']['quote'][0]['open']
+        high_prices = json_data['chart']['result'][0]['indicators']['quote'][0]['high']
+        low_prices = json_data['chart']['result'][0]['indicators']['quote'][0]['low']
+        close_prices = json_data['chart']['result'][0]['indicators']['quote'][0]['close']
+
+        df = pd.DataFrame({
+            "Time Frame": timestamp,
+            "Open": open_prices,
+            "High": high_prices,
+            "Low": low_prices,
+            "Close": close_prices
+        })
+
+        ist = timezone('Asia/Kolkata')
+        df['Time Frame'] = pd.to_datetime(df['Time Frame'], unit='s').dt.tz_localize('UTC').dt.tz_convert(ist)
+        df['Time Frame'] = pd.to_datetime(df['Time Frame'], unit='s')
+            
+        result_df = pd.DataFrame(df)
+        current_date = datetime.now().date()
+        
+        if not result_df.empty:
+            result_df['Time Frame'] = pd.to_datetime(result_df['Time Frame'], format='%H:%M:%S').apply(lambda x: x.replace(year=current_date.year, month=current_date.month, day=current_date.day))
+            result_df.rename(columns={'Time Frame': 'Date'}, inplace=True)
+            result_df.set_index('Date', inplace=True)
+            todays_data = result_df.rename_axis('Datetime').reset_index()
+            
+            df = pd.DataFrame(todays_data)
+            df['Datetime'] = pd.to_datetime(df['Datetime'])
+            df.set_index('Datetime', inplace=True)
+            
+            data = fetch_data()
+            df = pd.concat([data, df], ignore_index=False)
+            return df
+    
+    return pd.DataFrame({})
 
 def atr(DF,n):
     df=DF.copy()
@@ -70,58 +137,54 @@ def st_changes(data):
     fig, axes = mpl.plot(data, type='candle', style=style, addplot=[mpl.make_addplot(data['supertrend'], color='blue')],figsize=(16, 8) ,returnfig=True)
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     filename = f"mplfinance_chart_{timestamp}.png"
-    fig.savefig(filename)  
+    fig.savefig(filename)
     send_img(filename)
-    send_message("*SuperTrend Direction Changed on NSEI @ [15 min]*")
+    send_message("*SuperTrend Direction Changed on ^NSEI @ [15 min]*")
     
 
-def check_dates(data_frame, st_changes, data):
-    today = datetime.now().date()
-
-    yesterday = today - timedelta(days=1)
-    day_before_yesterday = today - timedelta(days=2)
-
-    if today in data_frame.index or yesterday in data_frame.index in data_frame.index:
-        st_changes(data)
-
-
 def main():
-    data = fetch_data()
+    data = fetch_todays_data_from_YF()
+    
+    if len(data) == 0:
+        return
+    
     n=4
     m=18
     data['supertrend']=supertrend(data,n,m)
-    add=mpl.make_addplot(data["supertrend"],color='blue')
-    #mpl.plot(data,addplot=add,type='candle',style="yahoo")
-
-
+    
+    if 'Datetime' not in data.columns:
+        data = add_datetime_column(data)
+    
+    data.set_index('Datetime', inplace=True)
+    add = mpl.make_addplot(data["supertrend"], color='blue')
+    
     data['supertrend_diff'] = data['supertrend'].diff().abs()
-    threshold = 300  # For example, a change of 100
-    sharp_change_df = data[data['supertrend_diff'] > threshold]
-    print(sharp_change_df)
-    check_dates(sharp_change_df, st_changes, data)
+    threshold = 300
+    
+    if data.iloc[-1]['supertrend_diff'] > threshold:
+        st_changes(data)
+        time.sleep(15 * 60)
 
 
-# Looper
-def check_and_call_function():
-    current_time = datetime.now().time()
-    
-    print("\r" +"Time: "+ str(current_time), end='', flush=True)
-    
-    hour = 9
-    
-    start_timek = datetime_time(hour, 30)
-    end_timek = datetime_time(hour, 32)
-            
-    if start_timek <= current_time <= end_timek:
-        print(" ")
-        main()
-        print("Re-Run at " + str(current_time))
-        time.sleep(2 * 60)
+def add_datetime_column(data):
+    if 'Datetime' not in data.columns:
+        data['Datetime'] = pd.date_range(start=str(data.iloc[0].name), periods=len(data), freq='15min')  # Example date range
+    return data 
+
 
 
 while True:
-    check_and_call_function()
-    time.sleep(60)  # 60-second wait
+    current_time = datetime.now().time()
+    print("\r" +"Time: "+ str(current_time), end='', flush=True)
+    print(" ")
+    
+    for hour in range(9, 16):  # From 9 AM to 3 PM
+        start_timei = datetime_time(hour, 15)                
+        if start_timei <= current_time :
+            main()
+            break
+    
+    time.sleep(2 * 60)  # 60-second wait
 
 
 
